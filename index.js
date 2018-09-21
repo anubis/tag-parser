0d7a0e22b6da595565c7b6be2bc43d797e05dc80
Josh Junon <josh@junon.me>
2018-09-20 20:41:21 -0600

    add way to debug parser

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ const pegjs = require('pegjs');
 
 const tagGrammar = fs.readFileSync(path.join(__dirname, 'tag.pegjs'), 'utf-8');
 
-const tagParser = pegjs.generate(tagGrammar, {
+const pegOpts = {
 	allowedStartRules: ['tagfile']
-});
+}
+
+const tagParser = pegjs.generate(tagGrammar, pegOpts);
+
+if ('DEBUG_TAG_PARSER' in process.env) {
+	const fs = require('fs');
+	fs.writeFileSync('/tmp/tag-parser.js', pegjs.generate(tagGrammar, {
+		...pegOpts,
+		output: 'source'
+	}));
+}
 
 function parseTagfile(source) {
 	return tagParser.parse(source + '\n');
